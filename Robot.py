74b38c4f0e827a0fd35c58ef9f1fd01ccf52c5b9
Raghav Verma <verma26raghav@gmail.com>
2019-10-07 02:01:24 +1100

    Initial function has been divided into 2 parts to allow reusability

--- Robot.py
+++ Robot.py
@@ -7,15 +7,15 @@ position = [0, 0]
 
 # Move Commands
 move_cmd = {
-    "F": [0, 1],          # F - move forward 1 unit
-    "B": [0, -1],         # B - move backward 1 unit
-    "R": [1, 0],          # R - turn right 90 degrees
-    "L": [-1, 0]          # L - turn left 90 degrees
+    "F": [0, 1],  # F - move forward 1 unit
+    "B": [0, -1],  # B - move backward 1 unit
+    "R": [1, 0],  # R - turn right 90 degrees
+    "L": [-1, 0]  # L - turn left 90 degrees
 }
 
 
-# Creating a function to calculate the minimum distance a robot needs to traverse in order to reach the starting point
-def min_distance(my_args):
+# Creating a function to calculate the destination point of the robot
+def destinationcoordinate(my_args):
     # Splitting the commands
     for i in my_args:
         parts = i.split(",")
@@ -30,19 +30,29 @@ def min_distance(my_args):
             position[0] += move_cmd[direction][0] * int(unit)
             position[1] += move_cmd[direction][1] * int(unit)
 
+    # Destination Point of the Robot
+    print("Destination point of the Robot is: ", position)
+    return position
+
+
+# Creating a function to calculate the minimum distance a robot needs to traverse in order to reach the starting point
+def min_distance(coordinates):
     # Calculating Distance
-    distance = abs(position[0]) + abs(position[1])
+    distance = abs(coordinates[0]) + abs(coordinates[1])
     print("The minimum amount of distance to get back to the starting point is:", distance)
 
 
 def main():
     # creating a parser object
-    myparser = argparse.ArgumentParser(description="Calculating Robot's Distance from Starting Point")
+    myparser = argparse.ArgumentParser(description="Calculating minimum amount of units the robot will need to "
+                                                   "traverse in order to get back to it's starting point")
 
     # adding argument
     myparser.add_argument("commands", nargs='*', metavar="string", type=str,
-                          help="All the commands separated by comma will be parsed to the output as the mimimum "
-                               "distance to the origin point")
+                          help="All the commands separated by comma will be parsed to the output as the minimum "
+                               "distance to the origin point. "
+                               "Example of providing inputs:"
+                               " F1,R1,B2,L1,B3")
 
     # parsing the arguments from the standard input
     my_args = myparser.parse_args()
@@ -50,7 +60,15 @@ def main():
     # check if command argument has any input data.
     # If it has, then calculate the robot's distance from the starting point
     if len(my_args.commands) != 0:
-        min_distance(my_args.commands)
+        # Calling function to compute the destination coordinates
+        coordinates = destinationcoordinate(my_args.commands)
+
+        # Calling function to compute the minimum distance back to source
+        min_distance(coordinates)
+        quit()
+    else:
+        print("Please enter alteast one command. Use filename -h for more instructions.")
+        quit()
 
 
 if __name__ == '__main__':
